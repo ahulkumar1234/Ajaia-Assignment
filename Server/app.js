a8101a5d994488fdeab2be91c9ccbb76df422db9
Rahul Kumar <rahulkumar8340527941@gmail.com>
2026-06-12 09:44:46 +0530

    url added

--- Server/app.js
+++ Server/app.js
@@ -7,7 +7,7 @@ const connectDB = require("./config/db");
 connectDB();
 const app = express();
 
-app.use(cors());
+app.use(cors({ origin: ['http://localhost:3000', 'https://ajaia-assignment.vercel.app'], credentials: true }));
 app.use(express.json());
 
 
